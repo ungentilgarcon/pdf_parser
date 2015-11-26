444ec51def6c2dd33e14c42260f1b0b186b59023
ungentilgarçon <gregory.bahde@laposte.net>
2015-11-26 13:01:54 +0100

    working2

--- pdfextract.js
+++ pdfextract.js
@@ -5,34 +5,25 @@ var fs = require('fs');
 var util = require('util');
 var lines = require('underscore');
 
-///to split with a param list
+
 var multiSplit = function(str, delimeters) {
-        //var result = [str];
+
         var result = str;
         if (typeof(delimeters) == 'string')
             delimeters = [delimeters];
             console.log(delimeters);
-            //console.log(str);
-            //console.log(result)
-              //console.log(delimeters.length);
+
         while (delimeters.length > 0) {
-            //console.log("result.length",result.length)
+
             for (var i = 0; i < result.length-1; i++) {
-                //console.log(i)
+
                 var tempSplit = result[i].split(delimeters[0]);
                 result = result.slice(0, i).concat(tempSplit).concat(result.slice(i + 1));
-            //console.log(tempSplit);
+
             }
             delimeters.shift();
         }
-        // //for (var i = 0; i < result.length-1; i++) {
-        //     for (var i = 0; i < result.length-1; i++) {
-        //     if (result[i]== "," && result[i+1] == ",")
-        //         //console.log(result.slice(0, i-1))
-        //         //console.log(result.slice(i+1, result.length))
-        //         result.slice(0, i-1).concat(result.slice(i+1, result.length))
-                
-        //     }
+
 
         return result;
     }
@@ -47,9 +38,8 @@ var bookWordCollection = extract(myfilePath, { splitPages: false }, function (er
         console.dir(err)
         return
     }
-    //console.log(pages.toString());
-    var mybook = multiSplit(text.toString(), splitList);
-    var mybook2 =mybook.replace(/^[,\s]+|[,\s]+$/g, '').replace(/,[,\s]*,/g, ',');
-        // console.log(toString(bookWordCollection))
-    console.log(mybook2);
+
+    var mybook = multiSplit(text.toString(), splitList).replace(/^[,\s]+|[,\s]+$/g, '').replace(/,[,\s]*,/g, ',');
+   
+    console.log(mybook);
 })
