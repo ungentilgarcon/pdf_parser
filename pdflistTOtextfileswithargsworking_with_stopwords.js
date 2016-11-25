8f2e2c978d8025e06eb2b8cfb4251a847f8128da
ungentilgarcon <monsieur.goonie@gmail.com>
2016-11-25 10:05:42 +0100

    working on english stopwords now

--- pdflistTOtextfileswithargsworking_with_stopwords.js
+++ pdflistTOtextfileswithargsworking_with_stopwords.js
@@ -16,52 +16,43 @@ Here’s the code:
  *   Usage: string_variable.removeStopWords();
  *   Output: The original String with stop words removed
  */
-String.prototype.removeStopWords = function() {
-    var x;
-    var y;
-    var word;
-    var stop_word;
-    var regex_str;
-    var regex;
-    var cleansed_string = this.valueOf();
+
   /*  var stop_words = new Array(*/
+
+
+String.isStopWord = function(word)
+{
   var stopath = path.join(__dirname, 'stopwords_english')
       var textstop = fs.readFileSync(stopath).toString();
-var stop_words = textstop.split("\n")
-
-           console.log("stop-words",stop_words);
-
-    // Split out all the individual words in the phrase
-    words = cleansed_string.match(/[^\s]+|\s+[^\s+]$/g)
-
-    // Review all the words
-    for(x=0; x < words.length; x++) {
-        // For each word, check all the stop words
-        for(y=0; y < stop_words.length; y++) {
-            // Get the current word
-            word = words[x].replace(/\s+|[^a-z]+/ig, "");   // Trim the word and remove non-alpha
-
-            // Get the stop word
-            stop_word = stop_words[y];
-
-            // If the word matches the stop word, remove it from the keywords
-            if(word.toLowerCase() == stop_word) {
-                // Build the regex
-                regex_str = "^\\s*"+stop_word+"\\s*$";      // Only word
-                regex_str += "|^\\s*"+stop_word+"\\s+";     // First word
-                regex_str += "|\\s+"+stop_word+"\\s*$";     // Last word
-                regex_str += "|\\s+"+stop_word+"\\s+";      // Word somewhere in the middle
-                regex = new RegExp(regex_str, "ig");
-
-                // Remove the word from the keywords
-                cleansed_string = cleansed_string.replace(regex, " ");
-            }
-        }
-    }
-    return cleansed_string.replace(/^\s+|\s+$/g, "");
-
+var stopWords = textstop.split("\n").toString()
+  /*console.log("stop-words",stopWords);*/
+	var regex = new RegExp("\\b"+word+"\\b","i");
+	if(stopWords.search(regex) < 0)
+	{
+		return false;
+	}else
+	{
+		return true;
+	}
 }
 
+String.prototype.removeStopWords = function()
+{
+	words = new Array();
+
+	this.replace(/\b[\w]+\b/g,
+			function($0)
+			{
+				if(!String.isStopWord($0))
+				{
+					words[words.length] = $0.trim();
+				}
+			}
+		);
+	return words.join(" ");
+}
+
+    // Split out all the individual words in the phrase
 
 
 
@@ -97,7 +88,7 @@ fs.readdir(testFolder, function(err, files) {
     var myfile = path.join( args, file);
 //ON extraie du pdf vers un fichier txt
       var myfileOUT = path.join(args, file).slice(0, -3).concat("txt").toString();
-console.log("myfileOUT",myfileOUT);
+/*console.log("myfileOUT",myfileOUT);*/
       var bookWordCollection = extract(myfile, {
         splitPages: false
         }, function(err, text) {
@@ -106,8 +97,8 @@ console.log("myfileOUT",myfileOUT);
                 console.dir(err)
                 return
                 }
-
-            fs.writeFile(myfileOUT, text.toString().removeStopWords() , function(err) {
+                //console.log("TEXTTEXTTEXT",text);
+            fs.writeFile(myfileOUT, text/*.toString().removeStopWords()*/ , function(err) {
               if(err) {
                 return console.log(err);
               }
@@ -118,22 +109,23 @@ console.log("myfileOUT",myfileOUT);
 
 
 
-            console.log("The file was saved!");
+
         })
           //ON ouvre le fichier complet, et on extrait les STOPwords
-/*        var fulltext =fs.readFile(myfileOUT, 'utf8', function (err,data) {
+        var fullT =fs.readFile(myfileOUT, 'utf8' ,function (err,data) {
            if (err) {
              return console.log(err);
            }
+           fulltext = data
+           console.log(data);
+           var myfileOUT2 = path.join(args, file).slice(0, -4).concat("SANSSTOP.txt").toString();
+           fs.writeFile(myfileOUT2, fulltext.toString().removeStopWords() , function(err) {
+             if(err) {
+               return console.log(err);
+             }
+           })
 
          });
-         console.log("fulltext",fulltext);
-         var myfileOUT2 = path.join(args, file).slice(0, -4).concat("SANSSTOP.txt").toString();
-         fs.writeFile(myfileOUT2, fulltext.removeStopWords() , function(err) {
-           if(err) {
-             return console.log(err);
-           }
-         })*/
 
 
 
