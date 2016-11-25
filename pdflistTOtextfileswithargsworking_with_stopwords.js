9899d2b8b965e29b340b80dd98b796e09fd5fdf0
ungentilgarcon <monsieur.goonie@gmail.com>
2016-11-25 15:27:48 +0100

    that's working -spanish / fr /en \n  usage:  node pdflistTOtextfileswithargsworking_with_stopwords.js "/pdfcontainingfolder" en

--- pdflistTOtextfileswithargsworking_with_stopwords.js
+++ pdflistTOtextfileswithargsworking_with_stopwords.js
@@ -1,28 +1,24 @@
 //FONCTION POUR OTER LES STOPSWORDS
-/*foo = "here is a string that has some stop words in it";
-bar = foo.removeStopWords();
-// The value of bar will be "string stop words"
+//from https://github.com/mirzaasif/JS-StopWord/blob/master/StopWord.js
 
-The method works by first defining a very large array with stop words in it. The string itself is split by whitespaces within the string, creating an array of contiguous non-whitespace characters. There are then two loops for processing the words.
-
-The outer loop cycles through all of the words in the string, and the inner loop cycles through all of the stop words. The inner loop performs a comparison between the current word and current stopword. If the word matches the stop word, all instances of it are removed from the main string.
-
-Here’s the code:
-?*/
-/*
- * String method to remove stop words
- * Written by GeekLad http://geeklad.com
- * Stop words obtained from http://www.lextek.com/manuals/onix/stopwords1.html
- *   Usage: string_variable.removeStopWords();
- *   Output: The original String with stop words removed
- */
-
-  /*  var stop_words = new Array(*/
-
-
-String.isStopWord = function(word)
+String.isStopWord = function(word, args2)
 {
-  var stopath = path.join(__dirname, 'stopwords_english')
+//console.log("args2",args2);
+  switch(args2) {
+      case "fr":
+var stopath = path.join(__dirname, 'stopwords_french')
+          break;
+      case "en":
+var stopath = path.join(__dirname, 'stopwords_english')
+          break;
+      case "es":
+var stopath = path.join(__dirname, 'stopwords_spanish')
+      default:
+  var stopath = path.join(__dirname, 'stopwords_english');
+  }
+
+
+
       var textstop = fs.readFileSync(stopath).toString();
 var stopWords = textstop.split("\n").toString()
   /*console.log("stop-words",stopWords);*/
@@ -43,7 +39,7 @@ String.prototype.removeStopWords = function()
 	this.replace(/\b[\w]+\b/g,
 			function($0)
 			{
-				if(!String.isStopWord($0))
+				if(!String.isStopWord($0, args2))
 				{
 					words[words.length] = $0.trim();
 				}
@@ -71,6 +67,8 @@ var lines = require('underscore');
 //RECUPERER LES ARGUMENTS
 var args = process.argv[2];
 console.log("args",args);
+var args2 = process.argv[3];
+console.log("args2",args2);
 const testFolder = args;
 const fs = require('fs');
 
@@ -103,11 +101,8 @@ fs.readdir(testFolder, function(err, files) {
                 return console.log(err);
               }
 
-            console.log("The file was saved!");
+            console.log("The file ".concat(myfileOUT).concat(" was saved!"));
             })
-
-
-
 
 
         })
@@ -117,20 +112,16 @@ fs.readdir(testFolder, function(err, files) {
              return console.log(err);
            }
            fulltext = data
-           console.log(data);
+          // console.log(data);
            var myfileOUT2 = path.join(args, file).slice(0, -4).concat("SANSSTOP.txt").toString();
            fs.writeFile(myfileOUT2, fulltext.toString().removeStopWords() , function(err) {
              if(err) {
                return console.log(err);
              }
+             console.log("The file ".concat(myfileOUT2).concat(" was saved!"));
            })
 
          });
-
-
-
-
-
     })
 
 
